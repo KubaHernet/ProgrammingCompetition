88965ff31e519ee433c75849b5160441d27da344
Kuba Hernet <jakub.hernet@gmail.com>
2021-06-27 11:47:07 +0200

    init backend project

---
+++ PCBack/PCBack.Web/Controllers/TasksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PCBack.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCBack.Web.Controllers
+{
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet("[controller]/{id}")]
+        public ActionResult<CompetitionTask> Get(string id)
+        {
+            var task = new CompetitionTask
+            {
+                Id = "id",
+                Description = "description",
+                Name = "name"
+            };
+
+            return Ok(task);
+        }
+    }
+}
